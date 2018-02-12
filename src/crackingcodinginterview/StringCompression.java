e57b1a38af7ac61584120eb57481b3490e126d67
asontireddy <36207110+asontireddy@users.noreply.github.com>
2018-02-12 12:28:48 -0600

    Update StringCompression.java

--- src/crackingcodinginterview/StringCompression.java
+++ src/crackingcodinginterview/StringCompression.java
@@ -1,47 +1,26 @@
-/*
- *Program to implement Strinbg Compression 
- * ex :aabcccccaaa--->a2b1c5a3
- * if compressed string length > original return original 
- * else return compressed
- */
-package crackingcodinginterview;
-import java.util.*;
+import java.util.Scanner;
 
-/**
- *
- * @author asontireddy
- */
 public class StringCompression 
 {
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println(compressedString(s));
-    }
-    private static String compressedString(String s)
-    {
-        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-        for(int i=0;i<s.length();i++)
-        {
-            if(map.containsKey(s.charAt(i)))
-                map.put(s.charAt(i),map.get(s.charAt(i))+1);
-            else
-                map.put(s.charAt(i),1);
-        }
-        StringBuilder sb = new StringBuilder("");
-        Iterator it = map.entrySet().iterator();
-        while (it.hasNext())
-        {
-        Map.Entry pair = (Map.Entry)it.next();
-        sb.append(pair.getKey());
-        sb.append(pair.getValue());
-        it.remove(); // avoids a ConcurrentModificationException
-        }  
-        if(sb.toString().length()>=s.length())
-            return s;
-        else
-            return sb.toString();
-        
-    }
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		StringBuilder sb= new StringBuilder("");
+		int count =0;
+		for(int i=0;i<s.length();i++)
+		{
+			count++;
+			if(i+1 >= s.length()||s.charAt(i)!=s.charAt(i+1))
+			{
+				sb.append(s.charAt(i));
+				sb.append(count);
+				count=0;
+			}	
+		}
+		if(sb.toString().length()<s.length())
+			System.out.println(sb.toString());
+		else
+			System.out.println(s);
+	}
 }
